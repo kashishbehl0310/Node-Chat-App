d34394c08a940045d312b98fdb36a16f7cec089f
kashishbehl0310 <kashishbehl36@gmail.com>
2018-07-29 16:42:23 +0530

    Web Sockets added

--- server/server.js
+++ server/server.js
@@ -1,13 +1,26 @@
 const path = require('path')
 const express = require('express')
+const SocketIO = require('socket.io')
+const http = require('http')
 
 const publicPath = path.join(__dirname, '../public')
 const app = express()
+var server = http.createServer(app)
+var io = SocketIO(server)
 
 app.use(express.static(publicPath))
 
+io.on('connection', (socket) => {
+    console.log('New User Connected')
+    socket.on('disconnect', () => {
+        console.log('User was disconnected')
+    })
+})
+
+
+
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`App listening on port ${port}`)
 })
 
