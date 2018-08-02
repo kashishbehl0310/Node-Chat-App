b8a33de6777cbe3a3bca3a75156c1dcfd96debe5
kashishbehl0310 <kashishbehl36@gmail.com>
2018-08-02 13:30:01 +0530

    Autoscroll

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,17 @@
 var socket = io();
+
+function scrollToBottom(){
+    var messages = $('#messages')
+    var newMessage = messages.children('li:last-child')
+    var clientHeight = messages.prop('clientHeight')
+    var scrollTop = messages.prop('scrollTop')
+    var scrollHeight = messages.prop('scrollHeight')
+    var newMessageHeight = newMessage.innerHeight()
+    var lastMessageHeight = newMessage.prev().innerHeight()
+    if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight)
+        messages.scrollTop(scrollHeight)
+}
+
 socket.on('connect', function() {
     console.log('Connected to server')
 })
@@ -20,7 +33,7 @@ socket.on('newMessage', function(message){
         createdAt: formattedTime
     })
     $('#messages').append(html)
-    
+    scrollToBottom()
 })
 
 socket.on('newLocationMessage', function(message){
@@ -38,6 +51,7 @@ socket.on('newLocationMessage', function(message){
     // a.attr('href', message.url)
     // li.append(a)
     $('#messages').append(html)
+    scrollToBottom()
 })
 
 $('#message-form').on('submit', function(e){
